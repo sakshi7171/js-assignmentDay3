13368e53b0ed0ad68ec53e7575fee5555731e331
sakshi7171 <61040929+sakshi7171@users.noreply.github.com>
2020-07-17 13:07:57 +0530

    Add files via upload

---
+++ text3.js
@@ -0,0 +1,8 @@
+//console.log("hello");
+
+let os = prompt("Enter the Operating system and version ");
+
+let version = prompt("enter the version");
+
+console.log(`Operating system  is ${os} and version is ${version}`)
+
