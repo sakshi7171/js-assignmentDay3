2eba7d1f1b0c4a3be2a397ad7d973dbb2650bf36
sakshi7171 <61040929+sakshi7171@users.noreply.github.com>
2020-07-17 13:08:46 +0530

    Update text3.js

--- text3.js
+++ text3.js
@@ -4,5 +4,5 @@ let os = prompt("Enter the Operating system and version ");
 
 let version = prompt("enter the version");
 
-console.log(`Operating system  is ${os} and version is ${version}`)
+console.log(`Operating system  is ${os} and version is ${version}`);
 
